ea48495a0d0beeff62053b3e691ca48b80da0c15
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-06-28 11:03:02 +0800

    features: create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<Signin />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { Navbar, TextInput, Button } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <Navbar className="border-b-2">
+      <Link to="/" className="self-center whitespace-nowrap">
+        WhisperWall
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button color="gray">Sign In</Button>
+        </Link>
+      </div>
+    </Navbar>
+  );
+};
+
+export default Header;
