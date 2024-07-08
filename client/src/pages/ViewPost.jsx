73d6e02c2894487ededaa96b468e2328ba0364d6
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-07-08 18:15:51 +0800

    change navigate after delete a post

--- client/src/pages/ViewPost.jsx
+++ client/src/pages/ViewPost.jsx
@@ -13,6 +13,8 @@ import {
   Spinner,
   Modal,
   FloatingLabel,
+  Tooltip,
+  Dropdown,
 } from "flowbite-react";
 import {
   HiClock,
@@ -21,10 +23,16 @@ import {
   HiTrash,
   HiOutlineTrash,
   HiOutlineExclamationCircle,
+  HiViewGrid,
+  HiSparkles,
+  HiOutlinePencilAlt,
+  HiViewGridAdd,
+  HiDotsCircleHorizontal,
+  HiDotsHorizontal,
 } from "react-icons/hi";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setShouldRefresh } from "../redux/refreshSlice.js";
+// import { setShouldRefresh } from "../redux/refreshSlice.js";
 
 const ViewPost = () => {
   const { postId } = useParams();
@@ -113,8 +121,8 @@ const ViewPost = () => {
         setErrMessage(data.message);
       } else {
         setSuccessMessage("Whisper deleted successfully");
-        dispatch(setShouldRefresh(true));
-        navigate(-1);
+        // dispatch(setShouldRefresh(true));
+        navigate("/dashboard?tab=whispers");
       }
     } catch (err) {
       setErrMessage(err.message);
@@ -177,134 +185,147 @@ const ViewPost = () => {
     );
   } else {
     return (
-      <div className="flex w-screen justify-center pt-10 pb-10">
-        <div className="flex flex-col gap-2 lg:w-2/5 max-w-full">
-          {errMessage && (
-            <Alert color="failure" icon={HiInformationCircle}>
-              <span className="font-medium">Oops!!</span>
-              {errMessage}
-            </Alert>
-          )}
-          {successMessage && (
-            <Alert color="success" icon={HiInformationCircle}>
-              {successMessage}
-            </Alert>
-          )}
-          {/* Post */}
-          <div className="max-w-full max-h-72">
-            <img
-              src={headerImage}
-              alt="header image"
-              className="w-full h-full object-cover rounded-md"
-            />
-          </div>
-          <div className="w-full flex justify-start items-center gap-3 pl-5 pr-5 lg:p-0">
-            <Badge>{tag}</Badge>
-            <Badge color="gray" icon={HiClock}>
-              {updatedTime}
-            </Badge>
-            <div className="font-serif">
-              from <span className="font-semibold">{pseudonym}</span>
-            </div>
-            {(currentUser._id === authorId || currentUser.isAdmin) && (
-              <>
-                <HiPencil
-                  className="h-5 w-5 ml-2 hover:text-pink-500"
-                  onClick={handleEditPost}
-                />
-                <HiTrash
-                  className="h-5 w-5 ml-2 hover:text-pink-500"
-                  onClick={() => {
-                    setItemToDel("post");
-                    setOpenModal(true);
-                  }}
-                />
-              </>
-            )}
-          </div>
-          <div
-            className="pl-5 pr-5 lg:p-0 prose mt-5 mb-5 font-serif"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
-          <HR.Text text="MESSAGE BOARD" />
-          {postComments.length !== 0 && (
-            <div className="text-gray-500 flex flex-col gap-3 p-2 font-thin">
-              {postComments.map((comment, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col border-2 border-dotted rounded-md p-2"
-                >
-                  <span className="font-3xl">{comment.content}</span>
-                  <div className="flex flex-col items-end">
-                    <span>--{comment.pseudonym}</span>
-                    <span>{formatTime(comment.createdAt)}</span>
-                    {currentUser.isAdmin && (
-                      <HiOutlineTrash
-                        className="h-6 w-6 mt-3 hover:text-pink-500"
-                        onClick={() => {
-                          setItemToDel("comment");
-                          setTempCommentId(comment._id);
-                          setOpenModal(true);
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-          <div className="flex max-w-full gap-3 items-center p-2">
-            {/* Comment */}
-            <form
-              className="w-full flex flex-col items-end"
-              onSubmit={handleAddComment}
+      <div>
+        <div className="fixed bottom-10 end-10">
+          <Tooltip content="Create a new whisper" style="dark" placement="left">
+            <Button
+              className="rounded-full w-12 h-12"
+              gradientMonochrome="pink"
+              onClick={() => navigate("/create-whisper")}
             >
-              <Textarea
-                rows={6}
-                onChange={(e) =>
-                  setComment({ ...comment, content: e.target.value })
-                }
-                required
+              <HiViewGridAdd className="w-6 h-6" />
+            </Button>
+          </Tooltip>
+        </div>
+
+        <div className="flex w-full justify-center pt-10 pb-10">
+          <div className="flex flex-col gap-2 lg:w-2/5 max-w-full">
+            {errMessage && (
+              <Alert color="failure" icon={HiInformationCircle}>
+                <span className="font-medium">Oops!!</span>
+                {errMessage}
+              </Alert>
+            )}
+            {successMessage && (
+              <Alert color="success" icon={HiInformationCircle}>
+                {successMessage}
+              </Alert>
+            )}
+            {/* Post */}
+            <div className="max-w-full max-h-72">
+              <img
+                src={headerImage}
+                alt="header image"
+                className="w-full h-full object-cover rounded-md"
               />
-              <div className="flex gap-3 items-center mt-5">
-                <FloatingLabel
-                  variant="outlined"
-                  label="Pseudonym"
+            </div>
+            <div className="w-full flex justify-start items-center gap-3 pl-5 pr-5 lg:p-0">
+              <Badge>{tag}</Badge>
+              <Badge color="gray" icon={HiClock}>
+                {updatedTime}
+              </Badge>
+              <div className="font-serif">
+                from <span className="font-semibold">{pseudonym}</span>
+              </div>
+              {(currentUser._id === authorId || currentUser.isAdmin) && (
+                <>
+                  <HiPencil
+                    className="h-5 w-5 ml-2 hover:text-pink-500"
+                    onClick={handleEditPost}
+                  />
+                  <HiTrash
+                    className="h-5 w-5 ml-2 hover:text-pink-500"
+                    onClick={() => {
+                      setItemToDel("post");
+                      setOpenModal(true);
+                    }}
+                  />
+                </>
+              )}
+            </div>
+            <div
+              className="pl-5 pr-5 lg:p-0 prose mt-5 mb-5 font-serif"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+            <HR.Text text="MESSAGE BOARD" />
+            {postComments.length !== 0 && (
+              <div className="text-gray-500 flex flex-col gap-3 p-2 font-thin">
+                {postComments.map((comment, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col border-2 border-dotted rounded-md p-2"
+                  >
+                    <span className="font-3xl">{comment.content}</span>
+                    <div className="flex flex-col items-end">
+                      <span>--{comment.pseudonym}</span>
+                      <span>{formatTime(comment.createdAt)}</span>
+                      {currentUser.isAdmin && (
+                        <HiOutlineTrash
+                          className="h-6 w-6 mt-3 hover:text-pink-500"
+                          onClick={() => {
+                            setItemToDel("comment");
+                            setTempCommentId(comment._id);
+                            setOpenModal(true);
+                          }}
+                        />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+            <div className="flex max-w-full gap-3 items-center p-2">
+              {/* Comment */}
+              <form
+                className="w-full flex flex-col items-end"
+                onSubmit={handleAddComment}
+              >
+                <Textarea
+                  rows={6}
                   onChange={(e) =>
-                    setComment({ ...comment, pseudonym: e.target.value })
+                    setComment({ ...comment, content: e.target.value })
                   }
+                  required
                 />
-                {/* <TextInput
+                <div className="flex gap-3 items-center mt-5">
+                  <FloatingLabel
+                    variant="outlined"
+                    label="Pseudonym"
+                    onChange={(e) =>
+                      setComment({ ...comment, pseudonym: e.target.value })
+                    }
+                  />
+                  {/* <TextInput
                   placeholder="Your pseudonym"
                   onChange={(e) =>
                     setComment({ ...comment, pseudonym: e.target.value })
                   }
                 /> */}
-                <Button
-                  gradientDuoTone="pinkToOrange"
-                  pill
-                  type="submit"
-                  disabled={addCommentLoading}
-                  className="-mt-2"
-                >
-                  {addCommentLoading ? (
-                    <>
-                      <Spinner size="sm" />
-                      <span className="pl-3">Loading...</span>
-                    </>
-                  ) : (
-                    "Send"
-                  )}
-                </Button>
-              </div>
-            </form>
-            {addCommentErr && <Alert color="failure">{errMessage}</Alert>}
-            {/* <TextInput
+                  <Button
+                    gradientDuoTone="pinkToOrange"
+                    pill
+                    type="submit"
+                    disabled={addCommentLoading}
+                    className="-mt-2"
+                  >
+                    {addCommentLoading ? (
+                      <>
+                        <Spinner size="sm" />
+                        <span className="pl-3">Loading...</span>
+                      </>
+                    ) : (
+                      "Send"
+                    )}
+                  </Button>
+                </div>
+              </form>
+              {addCommentErr && <Alert color="failure">{errMessage}</Alert>}
+              {/* <TextInput
               className="flex-1"
               onClick={() => setIsOpen(true)}
               placeholder="Leave your message..."
             /> */}
-            {/* <Drawer
+              {/* <Drawer
               open={isOpen}
               onClose={() => setIsOpen(false)}
               position="bottom"
@@ -350,39 +371,40 @@ const ViewPost = () => {
                 {addCommentErr && <Alert color="failure">{errMessage}</Alert>}
               </Drawer.Items>
             </Drawer> */}
-          </div>
-        </div>
-        <Modal
-          show={openModal}
-          size="md"
-          onClose={() => setOpenModal(false)}
-          popup
-        >
-          <Modal.Header />
-          <Modal.Body>
-            <div className="text-center">
-              <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Deleted content cannot be restored. Are you sure to delete it?
-              </h3>
-              <div className="flex justify-center gap-4">
-                <Button
-                  color="failure"
-                  onClick={() => {
-                    setOpenModal(false);
-                    itemToDel === "post" && handleDeletePost();
-                    itemToDel === "comment" && handleDeleteComment();
-                  }}
-                >
-                  {"Yes, I'm sure"}
-                </Button>
-                <Button color="gray" onClick={() => setOpenModal(false)}>
-                  No, cancel
-                </Button>
-              </div>
             </div>
-          </Modal.Body>
-        </Modal>
+          </div>
+          <Modal
+            show={openModal}
+            size="md"
+            onClose={() => setOpenModal(false)}
+            popup
+          >
+            <Modal.Header />
+            <Modal.Body>
+              <div className="text-center">
+                <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                  Deleted content cannot be restored. Are you sure to delete it?
+                </h3>
+                <div className="flex justify-center gap-4">
+                  <Button
+                    color="failure"
+                    onClick={() => {
+                      setOpenModal(false);
+                      itemToDel === "post" && handleDeletePost();
+                      itemToDel === "comment" && handleDeleteComment();
+                    }}
+                  >
+                    {"Yes, I'm sure"}
+                  </Button>
+                  <Button color="gray" onClick={() => setOpenModal(false)}>
+                    No, cancel
+                  </Button>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </div>
       </div>
     );
   }
