bd9a46e370e0ad071e84f87746bc5c67937c2814
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-07-10 16:22:09 +0800

    update explore pagination

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,11 @@ import HomeCard from "../components/HomeCard";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { setShouldRefresh } from "../redux/refreshSlice";
-import { HiOutlineRefresh, HiViewGridAdd } from "react-icons/hi";
+import {
+  HiChevronDoubleDown,
+  HiOutlineRefresh,
+  HiViewGridAdd,
+} from "react-icons/hi";
 import { Button, Spinner, Tooltip } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
 
@@ -14,20 +18,27 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [errMessage, setErrMessage] = useState(null);
   const navigate = useNavigate();
+  const [showMore, setShowMore] = useState(true);
 
   const getPosts = async () => {
     setErrMessage(null);
-    // const res = await fetch("/api/post/getposts");
     try {
       setLoading(true);
-      const res = await fetch("/api/post/get-random-posts");
+      const startIndex = posts.length;
+      const res = await fetch(`/api/post/getposts?startIndex=${startIndex}`);
+      // const res = await fetch("/api/post/get-random-posts");
       const data = await res.json();
       if (!res.ok) {
         setErrMessage(data.message);
         setLoading(false);
         return;
       }
-      setPosts(data.posts);
+      setPosts((prev) => [...prev, ...data.posts]);
+      if (data.posts.length < 12) {
+        setShowMore(false);
+      } else {
+        setShowMore(true);
+      }
       setLoading(false);
     } catch (err) {
       setErrMessage(err.message);
@@ -46,8 +57,16 @@ const Home = () => {
   //   }
   // }, [shouldRefresh]);
 
-  const refreshPage = () => {
-    window.location.reload();
+  // const refreshPage = () => {
+  //   window.location.reload();
+  // };
+
+  const handleShowMore = async () => {
+    if (showMore) {
+      const res = await getPosts();
+      return res.ok;
+    }
+    return;
   };
 
   return (
@@ -59,27 +78,31 @@ const Home = () => {
           color="pink"
         />
       )}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-        {posts.slice(0, 13).map((post) => (
-          <HomeCard key={post._id} post={post} />
-        ))}
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+          {posts.map((post) => (
+            <HomeCard key={post._id} post={post} />
+          ))}
+        </div>
+        {showMore && !loading && (
+          <HiChevronDoubleDown
+            className="text-rose-600 hover:text-rose-400 cursor-pointer w-10 h-10"
+            onClick={handleShowMore}
+          ></HiChevronDoubleDown>
+        )}
       </div>
 
       <div className="fixed bottom-10 end-10">
-        <Tooltip
-          content="Get new random whispers"
-          style="dark"
-          placement="left"
-        >
+        {/* <Tooltip content="Show more" style="dark" placement="left">
           <Button
             className="rounded-full w-12 h-12"
             gradientMonochrome="failure"
-            onClick={refreshPage}
+            onClick={() => handleShowMore()}
             disabled={loading}
           >
             <HiOutlineRefresh className="w-6 h-6" />
           </Button>
-        </Tooltip>
+        </Tooltip> */}
         <Tooltip content="Create a new whisper" style="dark" placement="left">
           <Button
             className="rounded-full w-12 h-12 mt-2"
